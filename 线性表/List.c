e876192230293e6386deacced85fbeacc89f0c54
yedushusheng <yedushusheng@163.com>
2018-11-12 21:47:26 +0800

    代码框架

---
+++ 线性表/List.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+#define MAXSIZE 10
+typedef struct SequceList{
+	char element[MAXSIZE];
+	int length;
+}SqList;
+
+/*获取元素*/
+int GetElement(){
+
+}
+
+/*插入元素*/
+int InsertElement(){
+	
+}
+
+/*删除元素*/
+int DeleteElement(){
+	
+}
+ 
+int main(){
+    
+}
