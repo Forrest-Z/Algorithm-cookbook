a95fb3b855a83327a0db7ca6c5f441febf2aee69
yedushusheng <yedushusheng@163.com>
2018-11-12 23:15:13 +0800

    运行获取链表结果

--- 线性表/List.c
+++ 线性表/List.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 #define MAXSIZE 10
 typedef struct SequceList{
-	char element[MAXSIZE];
+	int element[MAXSIZE];
 	int length;
 }SqList;
 
@@ -12,10 +13,12 @@ int GetElement(SqList L,int index){
     return L.element[index-1];
 }
 //方法二：
+/*
 bool GetElement(SqList L,int index,char *element){
     *element=L.element[index-1];
     return 0;
 }
+*/
 
 
 /*插入元素*/
@@ -23,11 +26,14 @@ int InsertElement(){
 	
 }
 
+
 /*删除元素*/
 int DeleteElement(){
 	
 }
+
  
 int main(){
-    
+    SqList L;
+    printf("result:%d",GetElement(L,1));
 }
