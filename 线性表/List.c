85c17a52ac011f4229113afb5efa8fbe80815f20
yedushusheng <yedushusheng@163.com>
2018-11-12 23:25:38 +0800

    Update 线性表/List.c

--- 线性表/List.c
+++ 线性表/List.c
@@ -6,19 +6,27 @@ typedef struct SequceList{
 	int element[MAXSIZE];
 	int length;
 }SqList;
+/*结构体定义方式： 
+1、SqList L;-->L.element[] , L.length
+2、SqList *L; -->L->element[] , L->lenght
+*/
 
 /*获取元素*/
-//方法一 
+//方法一 ： 
+/*
 int GetElement(SqList L,int index){
     return L.element[index-1];
 }
-//方法二：
-/*
-bool GetElement(SqList L,int index,char *element){
-    *element=L.element[index-1];
+*/
+
+//方法二：(推荐)
+int GetElement(SqList L,int index,int *e){
+	if(L.length==0 || index<0 || inde>L.length)
+	    return -1;
+    *e=L.element[index-1];
     return 0;
 }
-*/
+
 
 
 /*插入元素*/
@@ -35,5 +43,7 @@ int DeleteElement(){
  
 int main(){
     SqList L;
-    printf("result:%d",GetElement(L,1));
+    int *e; 
+    GetElement(L,1,e)
+    printf("result:%d",*e);
 }
